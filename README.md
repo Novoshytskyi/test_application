7c657f2cc006c2bd2896ec668c867b90365ba634
Anton Novoshytskyi <novtsm@gmail.com>
2022-07-07 22:41:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# test_application
+
+Anton Novoshytskyi's test task for the company Solid Software.
+
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _MyTestApp());
+}
+
+class _MyTestApp extends StatelessWidget {
+  const _MyTestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: _MyHomePage(),
+    );
+  }
+}
+
+class _MyHomePage extends StatefulWidget {
+  const _MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<_MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<_MyHomePage> {
+  // late Color _randomBackgroundColor;
+  Color? _randomBackgroundColor;
+
+  @override
+  void initState() {
+    _randomBackgroundColor = _generateColor();
+    super.initState();
+  }
+
+  void _setRandomBackgroundColor() {
+    setState(() {
+      _randomBackgroundColor = _generateColor();
+    });
+  }
+
+  Color _generateColor() {
+    // r, g, b - from 0 to 255 (256 * 256 * 256 = 16 777 216 colors)
+    const int maxCountOfColorParameter = 256;
+
+    return Color.fromRGBO(
+      Random().nextInt(maxCountOfColorParameter),
+      Random().nextInt(maxCountOfColorParameter),
+      Random().nextInt(maxCountOfColorParameter),
+      1,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double sizeTextOnScreen = 45.0;
+
+    return Scaffold(
+      body: GestureDetector(
+        onTap: _setRandomBackgroundColor,
+        child: Container(
+          color: _randomBackgroundColor,
+          alignment: Alignment.center,
+          child: Text(
+            'Hey there',
+            style: TextStyle(
+              color: Colors.amber[900],
+              fontSize: sizeTextOnScreen,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
